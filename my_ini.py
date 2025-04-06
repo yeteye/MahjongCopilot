7dea26e634aa3059cfc66a85e0e5ac8551f40111
Osprey <u202211707@hust.edu.cn>
2025-04-06 20:24:26 +0800

    default

---
+++ my_ini.py
@@ -0,0 +1,8 @@
+import json
+
+def init():
+    #清空my_simulate_gen_info.txt和pre.txt
+    with open('my_simulate_gen_info.txt', 'w') as f:
+        pass
+    with open('pre.txt', 'w') as f:
+        pass
--- my_main.py
+++ my_main.py
@@ -1,15 +1,12 @@
 from prompt_ui import _root
 from testThread import main
-# from medium import mid_func
 import threading
+from my_ini import init
 
 if __name__ == '__main__':
-    # 将main()放在一个单独的线程中
+    init()
+
     main_thread = threading.Thread(target=main, daemon=True)
     main_thread.start()
-    # main_thread = threading.Thread(target=mid_func, daemon=True)
-    # main_thread.start()
 
-
-    # 将_root.mainloop()放在主线程中
     _root.mainloop()
